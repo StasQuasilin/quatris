84173c12d51e586b16150be8d3f17b7315c8877b
StasQuasilin <stasvasilin@gmail.com>
2018-04-29 16:57:56 +0300

    Update for 29.04.2018 from DESKTOP-M24NFC7

---
+++ Assets/Scripts/NextView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextView : MonoBehaviour {
+
+    GameControl2 control;
+    public GameObject imagePrefab;
+    public GameObject parent;
+    
+    void Start() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                GameObject point = Instantiate(imagePrefab, parent.transform);
+                RectTransform r = point.GetComponent<RectTransform>();
+
+                r.position = new Vector2(i * 10, j * 10);
+            }
+        }
+    }
+
+
+}
